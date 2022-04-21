6537b9f17a4fcc9b9ed580b3f76f8e70ad5720f7
Cholo0022 <andres.bozzani@gmail.com>
2022-04-21 11:16:44 -0300

    Validaciones

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 import { valida } from "./validaciones.js";
 
 const inputs = document.querySelectorAll("input");
-<<<<<<< HEAD
 const textarea = document.querySelector("#mensaje")
 
 textarea.addEventListener("blur", (textarea)=>{
@@ -9,22 +8,9 @@ textarea.addEventListener("blur", (textarea)=>{
   console.log(textarea.target)
 })
 
-=======
->>>>>>> 561b125166689ddde8ae19745961927f9a5cc5a3
 
 inputs.forEach((input) => {
   input.addEventListener("blur", (input) => {
     valida(input.target);
-<<<<<<< HEAD
   });
 });
-=======
-    console.log(input.target);
-  });
-});
-
-const textarea = document.getElementById("mensaje");
-
-
-valida(textarea);
->>>>>>> 561b125166689ddde8ae19745961927f9a5cc5a3
