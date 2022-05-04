dbb22b36425e636db81b7993ca1f98e53bfb8bea
Cholo0022 <andres.bozzani@gmail.com>
2022-05-03 21:37:26 -0300

    Modificacion de producto en proceso

--- control/actualizar.producto.js
+++ control/actualizar.producto.js
@@ -1,25 +1,41 @@
 import { productoServices } from "../js/productos.service.js";
 
+const form = document.querySelector("[data-form]");
+
 const obtenerInformacion = () => {
-    const url = new URL(window.location);
-    const id = url.searchParams.get("id");
+  const url = new URL(window.location);
+  const id = url.searchParams.get("id");
 
-    const img = document.getElementById("img");
-    const nombre = document.getElementById("nombre");
-    const precio = document.getElementById("precio");
-    const descripcion = document.getElementById("descripcion");
-    const tipo = document.getElementById("tipo");
+  const nombre = document.getElementById("nombre");
+  const precio = document.getElementById("precio");
+  const descripcion = document.getElementById("descripcion");
+  const tipo = document.getElementById("tipo");
 
-    // console.log(img, nombre, precio, descripcion, tipo)
-
-    productoServices.detalleProducto(id).then(producto => {
-        img.value = producto.url;
-        nombre.value = producto.nombre;
-        precio.value = producto.precio;
-        descripcion.value = producto.descripcion;
-        tipo.value = producto.tipo;
-    });
-
-}
+  productoServices.detalleProducto(id).then((producto) => {
+    nombre.value = producto.nombre;
+    precio.value = producto.precio;
+    descripcion.value = producto.descripcion;
+    tipo.value = producto.tipo;
+  });
+};
 
 obtenerInformacion();
+
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const url = new URL(window.location);
+  const id = url.searchParams.get("id");
+
+  nombre.value = producto.nombre;
+  precio.value = producto.precio;
+  descripcion.value = producto.descripcion;
+  tipo.value = producto.tipo;
+  productoServices.actualizarProducto(
+    img,
+    nombre,
+    precio,
+    descripcion,
+    tipo,
+    id
+  );
+});
