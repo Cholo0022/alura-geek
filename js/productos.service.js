445d24f0177dcadc589767ac2dd6d3e5c391dc84
Cholo0022 <andres.bozzani@gmail.com>
2023-07-19 11:47:22 -0300

    Prueba

--- js/productos.service.js
+++ js/productos.service.js
@@ -4,7 +4,7 @@ const listaProductos = () =>
   );
 
 const crearProducto = (url, nombre, precio, descripcion, tipo) => {
-  return fetch("https://alura-geek-ms24.vercel.app/producto", {
+  return fetch("https://alura-geek-ms24-orcx2eg5m-cholo0022.vercel.app/?vercelToolbarCode=E2IVjpYkDEI9LT8/producto", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
